fb39cd5067840fda1e538dd6906dbd1b78c7b413
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-11-21 19:14:19 +0100

    Layout, constructores, getters y setter de las bicis

---
+++ app/src/main/java/com/example/transportezaragozaapi/bizi/Bizi.java
@@ -0,0 +1,27 @@
+package com.example.transportezaragozaapi.bizi;
+
+public class Bizi {
+
+    private String title, icon;
+
+    public Bizi(String title, String icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
