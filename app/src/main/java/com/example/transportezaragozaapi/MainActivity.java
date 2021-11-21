16608a5c73c11e9e099423590d9d5608eca53925
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-11-21 18:33:31 +0100

    Creación clase principal de bicis

--- app/src/main/java/com/example/transportezaragozaapi/MainActivity.java
+++ app/src/main/java/com/example/transportezaragozaapi/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.transportezaragozaapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.transportezaragozaapi.bizi.BiziActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    // Metodos para ir a las actividades
+    public void irBizi(View view) {
+        Intent irBizi = new Intent(this, BiziActivity.class);
+        startActivity(irBizi);
     }
 }
---
+++ app/src/main/java/com/example/transportezaragozaapi/bizi/BiziActivity.java
@@ -0,0 +1,16 @@
+package com.example.transportezaragozaapi.bizi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.transportezaragozaapi.R;
+
+public class BiziActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bizi);
+    }
+}
