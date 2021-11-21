fac48b5461dfe382da7217626724456ae116978c
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-11-21 19:49:08 +0100

    BiziActivity y muestra la lista de todas las estaciones

--- app/src/main/java/com/example/transportezaragozaapi/VolleySingleton.java
+++ app/src/main/java/com/example/transportezaragozaapi/VolleySingleton.java
@@ -1,4 +1,28 @@
 package com.example.transportezaragozaapi;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class VolleySingleton {
+
+    private RequestQueue requestQueue;
+    public static VolleySingleton instance;
+
+    private VolleySingleton(Context context) {
+        requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+
+        if(instance == null) {
+            instance = new VolleySingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return requestQueue;
+    }
 }
--- app/src/main/java/com/example/transportezaragozaapi/bizi/BiziActivity.java
+++ app/src/main/java/com/example/transportezaragozaapi/bizi/BiziActivity.java
@@ -6,9 +6,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.transportezaragozaapi.R;
+import com.example.transportezaragozaapi.VolleySingleton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class BiziActivity extends AppCompatActivity {
@@ -26,6 +36,52 @@ public class BiziActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
 
+        biziList = new ArrayList<>();
+
+        buscarBizi();
+    }
+
+
+    private void buscarBizi() {
+
+        String url = "https://zaragoza.es/sede/servicio/urbanismo-infraestructuras/estacion-bicicleta.json";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("result");
+
+                            for(int i=0; i<jsonArray.length(); i++) {
+                                JSONObject result = jsonArray.getJSONObject(i);
+                                String title = result.getString("title");
+                                String icon = result.getString("icon");
+
+                                Bizi bizi = new Bizi(title, icon);
+                                biziList.add(bizi);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        BiziAdapter adapter = new BiziAdapter(BiziActivity.this, biziList);
+                        recyclerView.setAdapter(adapter);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonObjectRequest);
     }
 }
