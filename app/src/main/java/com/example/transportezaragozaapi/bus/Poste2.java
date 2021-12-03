447c7b44cac1df87e49f72d963dd83f53f1f09e0
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-12-03 18:15:02 +0100

    Conexión enlace bus con bus extendido

---
+++ app/src/main/java/com/example/transportezaragozaapi/bus/Poste2.java
@@ -0,0 +1,18 @@
+package com.example.transportezaragozaapi.bus;
+
+public class Poste2 {
+
+    private String idPoste2;
+
+    public Poste2(String idPoste2) {
+        this.idPoste2 = idPoste2;
+    }
+
+    public String getIdPoste2() {
+        return idPoste2;
+    }
+
+    public void setIdPoste2(String idPoste2) {
+        this.idPoste2 = idPoste2;
+    }
+}
