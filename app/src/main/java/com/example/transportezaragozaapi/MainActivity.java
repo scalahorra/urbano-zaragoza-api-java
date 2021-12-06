19346b6114f6be4416c58c94f8b94b8c206bd267
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-12-06 12:39:37 +0100

    Splash screen mientras carga la aplicación

--- app/src/main/java/com/example/transportezaragozaapi/MainActivity.java
+++ app/src/main/java/com/example/transportezaragozaapi/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.CargaTheme);
         setContentView(R.layout.activity_main);
     }
 
