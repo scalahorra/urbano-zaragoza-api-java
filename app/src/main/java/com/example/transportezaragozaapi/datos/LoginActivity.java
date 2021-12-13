c7f8503db4d4ac5282a67d4c7b8eadfaa7502759
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-12-13 18:27:24 +0100

    Revisión de detalles de los recyclerview

--- app/src/main/java/com/example/transportezaragozaapi/datos/LoginActivity.java
+++ app/src/main/java/com/example/transportezaragozaapi/datos/LoginActivity.java
@@ -9,11 +9,20 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.transportezaragozaapi.MainActivity;
 import com.example.transportezaragozaapi.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -22,13 +31,44 @@ import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email, password;
+    private Button signUp, logIn;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.emailEditText);
+        password = (EditText) findViewById(R.id.passwordEditText);
+        signUp = (Button) findViewById(R.id.signUpButton);
+        logIn = (Button) findViewById(R.id.logInButton);
+
+        //Boton para registrarse
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Email: " + email.getText().toString());
+                System.out.println("Contraseña: " + password.getText().toString());
+
+
+            }
+        });
+
+        //Boton para iniciar sesion
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
     }
+
 
     @Override
     public void onBackPressed() {
