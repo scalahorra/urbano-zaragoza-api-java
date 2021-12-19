96628e4a8d9f0916df8afad72af7942db99aac44
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-12-19 20:10:02 +0100

    Metida imagen de usuario dependiendo de esConductor

--- app/src/main/java/com/example/transportezaragozaapi/perfil/PerfilActivity.java
+++ app/src/main/java/com/example/transportezaragozaapi/perfil/PerfilActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +25,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class PerfilActivity extends AppCompatActivity {
 
     TextView tv_nombrePerfil, tv_emailPerfil, tv_esConductor;
+    ImageView iv_fotoPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,7 @@ public class PerfilActivity extends AppCompatActivity {
         tv_nombrePerfil = findViewById(R.id.tv_nombrePerfil);
         tv_emailPerfil = findViewById(R.id.tv_emailPerfil);
         tv_esConductor = findViewById(R.id.tv_esConductor);
+        iv_fotoPerfil = findViewById(R.id.iv_fotoPerfil);
 
         // Email de referencia para buscar en la bd
         String emailRef = user.getEmail();
@@ -43,25 +46,29 @@ public class PerfilActivity extends AppCompatActivity {
         // Busqueda de un documento especifico de la bd
         DocumentReference documentReference = firebaseFirestore.collection("usuarios").document(emailRef);
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @SuppressLint("SetTextI18n")
             @Override
             public void onSuccess(@NonNull DocumentSnapshot documentSnapshot) {
                 if(documentSnapshot.exists()) {
-                    Toast.makeText(PerfilActivity.this, "Carga de datos correcta", Toast.LENGTH_SHORT).show();
 
                     // Asignacion datos a las variables
                     String nombre = documentSnapshot.get("nombre").toString();
                     String email = documentSnapshot.get("email").toString();
+                    String textoConductor;
                     boolean esConductor = documentSnapshot.get("esConductor").equals(true);
 
                     // Asignacion variables a los datos
                     tv_nombrePerfil.setText(nombre);
                     tv_emailPerfil.setText(email);
-                    if(esConductor) {
-                        tv_esConductor.setText("Es conductor");
-                    } else {
-                        tv_esConductor.setText("Es pasajero");
+                    if(esConductor){
+                        textoConductor = "Conductor";
+                        iv_fotoPerfil.setImageResource(R.drawable.chofer_imagen);
+                    } else{
+                        textoConductor = "Pasajero";
+                        iv_fotoPerfil.setImageResource(R.drawable.pasajero_imagen);
                     }
+                    tv_esConductor.setText(textoConductor);
+
+                    Toast.makeText(PerfilActivity.this, "Carga de datos correcta", Toast.LENGTH_SHORT).show();
 
                 } else {
                     Toast.makeText(PerfilActivity.this, "No se pudo cargar los datos", Toast.LENGTH_SHORT).show();
@@ -70,7 +77,7 @@ public class PerfilActivity extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-
+                Toast.makeText(PerfilActivity.this, "No se ha podido obtener el documento de la base de datos", Toast.LENGTH_SHORT).show();
             }
         });
     }
